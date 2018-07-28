e23185845a7c1c249cf07faed7888e0b88a1ba7f
Yolanda Fay <yolandafay@gmail.com>
2018-07-28 01:43:05 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,12 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+def display_board(move_array)
+  
+  
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
